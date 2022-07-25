b0a7a8a843edf64fd2207f56b7608df7d062f297
TSENTSEUS <tsentseus.askold@lll.kpi.ua>
2022-07-25 15:24:01 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InteractionComponent } from "./components/interaction/interaction.component";
+import { HttpClientModule } from "@angular/common/http";
+import {HeaderCurrencyComponent} from "./components/header/header.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderCurrencyComponent,
+    InteractionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
